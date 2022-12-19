bd9b2ef1001344fa54d71c3ac0f0b1571d73580e
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-12-19 12:16:14 -0500

    update npm modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -79,7 +79,10 @@ module.exports = {
   plugins: [
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
     // require('@tailwindcss/line-clamp'),
+    // require('@tailwindcss/container-queries'),
+    // require('@headlessui/tailwindcss'),
     plugin(function ({ addBase, addUtilities, addVariant, theme }) {
       addUtilities({
         '.content-auto': {
