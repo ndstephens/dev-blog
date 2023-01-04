46dc89f03e1a3b8c1acac44c874374da5a2ea179
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-01-04 18:15:05 -0500

    had a weird bug regarding 'fs' module

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   swcMinify: true,
   // trailingSlash: true,
   webpack(config) {
+    //* In case a weird 'fs' bug shows up again
+    // config.resolve = {
+    //   ...config.resolve,
+    //   fallback: {
+    //     fs: false,
+    //     path: false,
+    //     os: false,
+    //   },
+    // };
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
