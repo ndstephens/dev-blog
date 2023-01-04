0472b737170180aa0c75e6de7b5b6dbe2080b734
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-01-04 18:20:42 -0500

    category page post filtering and pagination using params (WIP)

--- pages/blog/latest/index.tsx
+++ pages/blog/latest/index.tsx
@@ -1,34 +1,134 @@
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
-import type { PostMeta } from 'src/postsApi/postConfig';
 
-import PageContentWrapper from '@ui/PageLayout/PageContentWrapper';
-import PageHeader from '@ui/PageLayout/PageHeader';
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { useIsClient } from 'usehooks-ts';
+import { z } from 'zod';
 
 // import PostPreview from '@ui/PostPreview';
 import {
   extractPostMeta,
   getAllPosts,
-  getSliceOfPosts,
+  getAllPostTopicsInUse,
   sortPostsByDate,
-} from 'src/postsApi/fetchLocalPosts';
+} from '@scripts/posts';
+import {
+  PostMeta,
+  PostTopic,
+  PostTopicsSchema,
+} from '@scripts/posts/postConfig';
+import PageContentWrapper from '@ui/PageLayout/PageContentWrapper';
+import PageHeader from '@ui/PageLayout/PageHeader';
+import { numClamp } from '@utils/numbers';
 
+//* =============================================
+//*              UTIL FUNCTIONS                 =
+//*==============================================
+const verifyTopicParam = (topicParam: unknown) => {
+  const topicParamSchema = PostTopicsSchema.optional();
+  const result = topicParamSchema.safeParse(topicParam);
+  return result.success ? result.data : undefined;
+};
+
+const verifyPageParam = (pageParam: unknown) => {
+  const pageParamSchema = z.coerce.number().optional();
+  const result = pageParamSchema.safeParse(pageParam);
+  return result.success ? result.data : undefined;
+};
+
+const getPostsAndPages = (
+  postsMeta: PostMeta[],
+  topicParam: string | undefined,
+  pageParam: number | undefined,
+  numPostsPerPage: number
+) => {
+  const allPostsOfTopic = postsMeta.filter((postMeta) =>
+    !topicParam ? true : postMeta.topics.includes(topicParam as PostTopic)
+  );
+  const maxNumOfPages = Math.ceil(allPostsOfTopic.length / numPostsPerPage);
+  const page = !pageParam
+    ? 1
+    : numClamp(Math.floor(pageParam), 1, maxNumOfPages);
+
+  // console.log('allPosts:', allPostsOfTopic);
+  // console.log('topicParam:', topicParam);
+  // console.log('pageParam:', pageParam);
+  // console.log('numPostsPerPage:', numPostsPerPage);
+  // console.log('maxNumPages:', maxNumOfPages);
+  // console.log('page:', page);
+
+  const startIndex = (page - 1) * numPostsPerPage;
+  const endIndex = startIndex + numPostsPerPage;
+
+  // console.log('start:', startIndex);
+
+  const posts = allPostsOfTopic.slice(startIndex, endIndex);
+
+  // console.log('posts', posts);
+
+  return {
+    posts,
+    page,
+    maxNumOfPages,
+  };
+};
+
+//* =============================================
+//*                 COMPONENT                   =
+//*==============================================
 export default function BlogLatestPage({
   postsMeta,
+  postTopics,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const isClient = useIsClient();
+  const { isReady, pathname, query } = useRouter();
+
+  const topicParam = verifyTopicParam(query?.topic);
+  const pageParam = verifyPageParam(query?.page);
+
+  const { posts, page, maxNumOfPages } =
+    isClient && isReady
+      ? getPostsAndPages(postsMeta, topicParam, pageParam, 10)
+      : { posts: [], page: 1, maxNumOfPages: 1 };
+
+  // TODO: use zod to verify that query.topic is a string, try to break url
+  // TODO: implement pagination with basic UI
+  // TODO: refactor all this logic into a custom hook
+
   return (
     <>
       <PageContentWrapper bgColor="bg-surfaceClr-2">
         <PageHeader>
-          <h1 className="text-7xl font-bold text-textClr-1">Hello</h1>
+          <h1 className="font-serif text-7xl tracking-wide text-textClr-1">
+            Latest:
+          </h1>
         </PageHeader>
       </PageContentWrapper>
       <PageContentWrapper>
         <section className="pt-8 pb-8">
           <ul>
-            {postsMeta.map((postMeta) => (
-              <li key={postMeta.title}>
-                {/* <PostPreview postMeta={postMeta} /> */}
-                <pre>{JSON.stringify(postMeta, null, 2)}</pre>
+            {postTopics.map((postTopic) => (
+              <li key={postTopic}>
+                <Link
+                  href={
+                    query?.topic === postTopic
+                      ? pathname
+                      : `${pathname}/?topic=${postTopic}`
+                  }
+                >
+                  <a>{postTopic}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <br />
+          <ul>
+            {posts.map((post) => (
+              <li key={post.title}>
+                {/* <PostPreview post={post} /> */}
+                <pre>{JSON.stringify(post, null, 2)}</pre>
               </li>
             ))}
           </ul>
@@ -38,18 +138,20 @@ export default function BlogLatestPage({
   );
 }
 
-/* =============================================
-              DATA FETCHING
-============================================= */
+//* =============================================
+//*            DATA FETCHING                    =
+//*==============================================
 interface StaticProps {
   postsMeta: PostMeta[];
+  postTopics: PostTopic[];
 }
 
 export const getStaticProps: GetStaticProps<StaticProps> = async () => {
   const allPosts = await getAllPosts();
   const sortedPosts = allPosts.sort(sortPostsByDate);
-  const recentPosts = getSliceOfPosts({ posts: sortedPosts });
-  const postsMeta = recentPosts.map(extractPostMeta);
+  const postsMeta = sortedPosts.map(extractPostMeta);
+
+  const postTopics = getAllPostTopicsInUse(allPosts);
 
   if (!allPosts) {
     return {
@@ -60,6 +162,7 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
   return {
     props: {
       postsMeta,
+      postTopics,
     },
   };
 };
